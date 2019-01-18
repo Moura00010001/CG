6017fc0838547d1985f97920264c1cb04741c807
Moura <45687668+Moura00010001@users.noreply.github.com>
2019-01-18 13:51:14 -0200

    Add files via upload

---
+++ Atividade 1/Projeto/main.cpp
@@ -0,0 +1,194 @@
+#include "main.h"
+
+//-----------------------------------------------------------------------------
+void MyGlDraw(void)
+{
+	//*************************************************************************
+	// Chame aqui as funções do mygl.h
+	//*************************************************************************
+
+    // Teste de PutPixel()
+    /*
+    tPixel pixel;
+
+    // Canto superior esquerdo
+    for(int i = 0; i < IMAGE_WIDTH/2; i = i + 7){
+
+        for(int j = 0; j < IMAGE_HEIGHT/2; j = j + 7){
+
+            pixel = {i, j, {255, 0, 0, 255}}; // Pixel vermelho
+            PutPixel(&pixel);
+
+        }
+    }
+
+    // Canto superior direito
+    for(int i = 256; i < IMAGE_WIDTH; i = i + 7){
+
+        for(int j = 0; j < IMAGE_HEIGHT/2; j = j + 7){
+
+            pixel = {i, j, {0, 255, 0, 255}}; // Pixel verde
+            PutPixel(&pixel);
+
+        }
+    }
+
+    // Canto inferior esquerdo
+    for(int i = 0; i < IMAGE_WIDTH/2; i = i + 7){
+
+        for(int j = 256; j < IMAGE_HEIGHT; j = j + 7){
+
+            pixel = {i, j, {0, 0, 255, 255}}; // Pixel azul
+            PutPixel(&pixel);
+
+        }
+    }
+
+    // Canto inferior direito
+    for(int i = 256; i < IMAGE_WIDTH; i = i + 7){
+
+        for(int j = 256; j < IMAGE_HEIGHT; j = j + 7){
+
+            pixel = {i, j, {255, 255, 255, 0}};
+            PutPixel(&pixel);
+
+        }
+    }*/
+
+    // Teste de DrawLine()
+    /*
+    // Primeiro octante
+    tPixel pixel1 = {256, 256, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel2 = {512, 256, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel3 = {512, 384, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel4 = {512, 512, {0, 255, 0, 255}}; // Pixel verde
+
+    DrawLine(&pixel1, &pixel2); // Ângulo de 0 grau
+    DrawLine(&pixel1, &pixel3); // Ângulo de 30 graus
+    DrawLine(&pixel1, &pixel4); // Ângulo de 45 graus
+
+    // Segundo octante
+    pixel1 = {256, 256, {255, 0, 0, 255}}; // Pixel vermelho
+    tPixel pixel5 = {384, 512, {255, 0, 0, 255}}; // Pixel vermelho
+    tPixel pixel6 = {256, 512, {255, 0, 0, 255}}; // Pixel vermelho
+
+    DrawLine(&pixel1, &pixel5); // Ângulo de 60 graus
+    DrawLine(&pixel1, &pixel6); // Ângulo de 90 graus
+
+    // Terceiro octante
+    pixel1 = {256, 256, {0, 0, 255, 255}}; // Pixel azul
+    tPixel pixel7 = {128, 512, {0, 0, 255, 255}}; // Pixel azul
+    tPixel pixel8 = {0, 512, {255, 255, 255, 0}}; // Pixel branco
+
+    DrawLine(&pixel1, &pixel7); // Ângulo de 120 graus
+    pixel1 = {256, 256, {255, 255, 255, 0}}; // Pixel branco
+    DrawLine(&pixel1, &pixel8); // Ângulo de 135 graus
+
+    // Quarto octante
+    pixel1 = {256, 256, {255, 255, 255, 0}}; // Pixel branco
+    tPixel pixel9 = {0, 384, {255, 255, 255, 0}}; // Pixel branco
+    tPixel pixel10 = {0, 256, {255, 255, 255, 0}}; // Pixel branco
+
+    DrawLine(&pixel1, &pixel9); // Ângulo de 150 graus
+    DrawLine(&pixel1, &pixel10); // Ângulo de 180 graus
+
+    // Quinto octante
+    pixel1 = {256, 256, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel11 = {0, 128, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel12 = {0, 0, {0, 255, 0, 255}}; // Pixel verde
+
+    DrawLine(&pixel1, &pixel11); // Ângulo de 210 graus
+    DrawLine(&pixel1, &pixel12); // Ângulo de 225 graus
+
+    // Sexto octante
+    pixel1 = {256, 256, {255, 0, 0, 255}}; // Pixel vermelho
+    tPixel pixel13 = {128, 0, {255, 0, 0, 255}}; // Pixel vermelho
+    tPixel pixel14 = {256, 0, {255, 0, 0, 255}}; // Pixel vermelho
+
+    DrawLine(&pixel1, &pixel13); // Ângulo de 240 graus
+    DrawLine(&pixel1, &pixel14); // Ângulo de 270 graus
+
+    // Sétino octante
+    pixel1 = {256, 256, {0, 0, 255, 255}}; // Pixel azul
+    tPixel pixel15 = {384, 0, {0, 0, 255, 255}}; // Pixel azul
+
+    DrawLine(&pixel1, &pixel15); // Ângulo de 300 graus
+
+    // Oitavo octante
+    pixel1 = {256, 256, {255, 255, 255, 0}}; // Pixel branco
+    tPixel pixel16 = {512, 0, {255, 255, 255, 0}}; // Pixel branco
+    tPixel pixel17 = {512, 128, {255, 255, 255, 0}}; // Pixel branco
+
+    DrawLine(&pixel1, &pixel16); // Ângulo de 315 graus
+    DrawLine(&pixel1, &pixel17); // Ângulo de 330 graus*/
+
+    // Teste InterpolaCor()
+    /*
+    // De vermelho para verde
+    tPixel pixel1 = {128, 128, {255, 0, 0, 255}}; // Pixel vermelho
+    tPixel pixel2 = {384, 128, {0, 255, 0, 255}}; // Pixel verde
+
+    DrawLine(&pixel1, &pixel2);
+
+    // De verde para azul
+    pixel1 = {128, 256, {0, 255, 0, 255}}; // Pixel verde
+    pixel2 = {384, 256, {0, 0, 255, 255}}; // Pixel azul
+
+    DrawLine(&pixel1, &pixel2);
+
+    // De azul para vermelho
+    pixel1 = {128, 384, {0, 0, 255, 255}}; // Pixel azul
+    pixel2 = {384, 384, {255, 0, 0, 255}}; // Pixel vermelho
+
+    DrawLine(&pixel1, &pixel2);*/
+
+    // Teste DrawTriangle()
+
+    // Primeiro vértice
+    tPixel pixel1 = {128, 384, {0, 255, 0, 255}}; // Pixel verde
+    // Segundo vértice
+    tPixel pixel2 = {256, 128, {0, 0, 255, 255}}; // Pixel azul
+    // Terceiro vértice
+    tPixel pixel3 = {384, 384, {255, 0, 0, 255}}; // Pixel vermelho
+
+    DrawTriangle(&pixel1, &pixel2, &pixel3);
+    PreencheTriangulo(&pixel1, &pixel2, &pixel3);
+
+    // Teste e Falha de segmentação
+    /*
+    tPixel pixel1 = {384, 384, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel2 = {768, 128, {0, 0, 255, 255}}; // Pixel azul
+    tPixel pixel3 = {768, 384, {255, 0, 0, 255}}; // Pixel vermelho
+
+    DrawTriangle(&pixel1, &pixel2, &pixel3);*/
+
+    // Teste Bresenham puro
+    /*
+    tPixel pixel1 = {256, 256, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel2 = {512, 256, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel3 = {512, 384, {0, 255, 0, 255}}; // Pixel verde
+    tPixel pixel4 = {512, 512, {0, 255, 0, 255}}; // Pixel verde
+
+    DrawLine(&pixel1, &pixel2); // Ângulo de 0 grau
+    DrawLine(&pixel1, &pixel3); // Ângulo de 30 graus
+    DrawLine(&pixel1, &pixel4); // Ângulo de 45 graus*/
+
+}
+
+//-----------------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+	// Inicializações.
+	InitOpenGL(&argc, argv);
+	InitCallBacks();
+	InitDataStructures();
+
+	// Ajusta a função que chama as funções do mygl.h
+	DrawFunc = MyGlDraw;
+
+	// Framebuffer scan loop.
+	glutMainLoop();
+
+	return 0;
+}
+
---
+++ Atividade 1/Projeto/mygl.h
@@ -0,0 +1,262 @@
+#ifndef _MYGL_H_
+#define _MYGL_H_
+
+#include "definitions.h"
+
+#include <math.h>
+
+//*****************************************************************************
+// Defina aqui as suas funções gráficas
+//*****************************************************************************
+
+typedef struct{
+
+    unsigned char R, G, B, A;
+
+} tRGBA;
+
+typedef struct{
+
+    int x, y; // Coordenadas na janela
+    tRGBA cor;
+
+} tPixel;
+
+typedef struct{
+
+    float w1, w2, w3;
+
+} tCoordenadas;
+
+int PutPixel(tPixel* pixel){
+
+    // Posição em memória de cada componente do pixel
+    int componente;
+
+    if(pixel->x < 0 || pixel->y < 0 ||
+       pixel->x > IMAGE_WIDTH || pixel->y > IMAGE_HEIGHT){
+
+       /* Tentativa inválida de acessar uma posição que não
+        consta nos limites do sistema de coordenadas da janela*/
+
+       return 1;
+
+    }
+
+    // Posição do primeiro byte de cada pixel
+    componente = (pixel->x*4) + (pixel->y*IMAGE_WIDTH*4);
+
+    FBptr[componente] = pixel->cor.R; // Componente R do pixel
+    FBptr[++componente] = pixel->cor.G; // Componente G do pixel
+    FBptr[++componente] = pixel->cor.B; // Componente B do pixel
+    FBptr[++componente] = pixel->cor.A; // Componente A do pixel
+
+    return 0;
+
+}
+
+tPixel InterpolaCor(float p, tPixel* pixel1, tPixel* pixel2){
+
+    tPixel pixel;
+    pixel.cor.R = p*(pixel1->cor.R) + (1 - p)*(pixel2->cor.R);
+    pixel.cor.G = p*(pixel1->cor.G) + (1 - p)*(pixel2->cor.G);
+    pixel.cor.B = p*(pixel1->cor.B) + (1 - p)*(pixel2->cor.B);
+    pixel.cor.A = p*(pixel1->cor.A) + (1 - p)*(pixel2->cor.A);
+
+    return pixel;
+
+}
+
+float Distancia(tPixel* pixel1, tPixel* pixel2){
+
+    return sqrt((pixel2->x - pixel1->x)*(pixel2->x - pixel1->x) + (pixel2->y - pixel1->y)*(pixel2->y - pixel1->y));
+
+}
+
+void DrawLine(tPixel* pixel1, tPixel* pixel2){
+
+    int dx = pixel2->x - pixel1->x;
+    int dy = pixel2->y - pixel1->y;
+    int cAng = 0;
+
+    int auxX, auxY;
+
+    float dParcial;
+    float dTotal = Distancia(pixel1, pixel2);
+
+    if(dx < 0){ // Caso ponto final < ponto inicial
+
+        DrawLine(pixel2, pixel1);
+        return;
+    }
+
+    if(dy < 0)
+        cAng = -1;
+    else
+        cAng = 1;
+
+    int d;//, incr_e, incr_nee;
+
+    tPixel pixel = *pixel1;
+
+    PutPixel(&pixel);
+
+    // Escolha dos octantes
+    if(dx >= cAng*dy){ // m <= 1
+
+        if(dy < 0){ // Caso y2 < y1
+            d = 2*dy + dx;
+            while(pixel.x < pixel2->x){
+
+                if(d < 0){ // Pixel E (East) escolhido
+                    d += 2*(dy + dx);
+                    pixel.x++;
+                    pixel.y--;
+                } else{ // Pixel NE (Northeast) escolhido
+                    d += 2*dy;
+                    pixel.x++; // Varia apenas no eixo x
+                }
+
+                auxX = pixel.x;
+                auxY = pixel.y;
+                dParcial = Distancia(&pixel, pixel2);
+                pixel = InterpolaCor(dParcial/dTotal, pixel1, pixel2);
+                pixel.x = auxX;
+                pixel.y = auxY;
+                PutPixel(&pixel);
+            }
+
+        } else{ // Caso y1 < y2
+            d = 2*dy - dx;
+            while(pixel.x < pixel2->x){
+
+                if(d < 0){ // Pixel E (East) escolhido
+                    d += 2*dy;
+                    pixel.x++; // Varia apenas no eixo x
+                } else{ // Pixel NE (Northeast) escolhido
+                    d += 2*(dy - dx);
+                    pixel.x++;
+                    pixel.y++;
+                }
+
+                auxX = pixel.x;
+                auxY = pixel.y;
+                dParcial = Distancia(&pixel, pixel2);
+                pixel = InterpolaCor(dParcial/dTotal, pixel1, pixel2);
+                pixel.x = auxX;
+                pixel.y = auxY;
+                PutPixel(&pixel);
+            }
+        }
+
+    } else{ // |m|>1
+
+        if(dy < 0){ // Caso y2<y1
+            d = dy + 2*dx;
+            while(pixel.y > pixel2->y){
+
+                if(d < 0){
+                    d += 2*dx;
+                    pixel.y--; // Varia apenas no eixo y
+                } else{
+                    d += 2*(dy + dx);
+                    pixel.x++;
+                    pixel.y--;
+                }
+
+                auxX = pixel.x;
+                auxY = pixel.y;
+                dParcial = Distancia(&pixel, pixel2);
+                pixel = InterpolaCor(dParcial/dTotal, pixel1, pixel2);
+                pixel.x = auxX;
+                pixel.y = auxY;
+                PutPixel(&pixel);
+            }
+
+        } else{ // Caso y1 < y2
+            d = dy - 2*dx;
+            while(pixel.y < pixel2->y){
+
+                if(d < 0){
+                    d += 2*(dy - dx);
+                    pixel.x++;
+                    pixel.y++;
+                } else{
+                    d += -2*dx;
+                    pixel.y++; // Varia apenas no eixo y
+                }
+
+                auxX = pixel.x;
+                auxY = pixel.y;
+                dParcial = Distancia(&pixel, pixel2);
+                pixel = InterpolaCor(dParcial/dTotal, pixel1, pixel2);
+                pixel.x = auxX;
+                pixel.y = auxY;
+                PutPixel(&pixel);
+            }
+        }
+    }
+
+    PutPixel(pixel2);
+
+} // Fim da função DrawLine()
+
+void DrawTriangle(tPixel* pixel1, tPixel* pixel2, tPixel* pixel3){
+
+    DrawLine(pixel1, pixel2);
+    DrawLine(pixel2, pixel3);
+    DrawLine(pixel3, pixel1);
+
+}
+
+void PreencheTriangulo(tPixel* pixel1, tPixel* pixel2, tPixel* pixel3){
+
+    float w1, w2, w3, numerador, denominador, cor;
+
+    tPixel pixel;
+
+    for(int i = 0; i < IMAGE_WIDTH; i++){
+
+        for(int j = 0; j < IMAGE_HEIGHT; j++){
+
+           numerador = (pixel2->y - pixel3->y)*(i - pixel3->x) + (pixel3->x - pixel2->x)*(j - pixel3->y);
+           denominador = (pixel2->y - pixel3->y)*(pixel1->x - pixel3->x) + (pixel3->x - pixel2->x)*(pixel1->y - pixel3->y);
+           if(numerador == 0){
+                w1 = 0;
+           } else{
+                w1 = numerador / denominador;
+           }
+
+           numerador = (pixel3->y - pixel1->y)*(i - pixel3->x) + (pixel1->x - pixel3->x)*(j - pixel3->y);
+           if(numerador == 0){
+                w2 = 0;
+           } else{
+                w2 = numerador / denominador;
+           }
+
+           w3 = 1 - w1 - w2;
+
+           if(w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1){
+
+                pixel.x = i;
+                pixel.y = j;
+                cor = (w1*pixel1->cor.R + w2*pixel2->cor.R + w3*pixel3->cor.R) + 0.5;
+                pixel.cor.R = (char) cor;
+                cor = (w1*pixel1->cor.G + w2*pixel2->cor.G + w3*pixel3->cor.G) + 0.5;
+                pixel.cor.G = (char) cor;
+                cor = (w1*pixel1->cor.B + w2*pixel2->cor.B + w3*pixel3->cor.B) + 0.5;
+                pixel.cor.B = (char) cor;
+                cor = (w1*pixel1->cor.A + w2*pixel2->cor.A + w3*pixel3->cor.A) + 0.5;
+                pixel.cor.A = (char) cor;
+
+                PutPixel(&pixel);
+
+           }
+        }
+    }
+
+}
+
+
+#endif // _MYGL_H_
+
